a0e678b5295d04978a19aebd71516109a97a64d1
vickykumar1001 <vicky9060993097@gmail.com>
2024-11-02 16:25:22 +0530

    Fixed import for about page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,5 +2,4 @@ import Error from './Error';
 import Landing from './Landing';
 import ProtectedRoute from './ProtectedRoute';
 import Register from './Register';
-import About from './About';
-export { Landing, Error, Register, ProtectedRoute, About };
+export { Landing, Error, Register, ProtectedRoute };
