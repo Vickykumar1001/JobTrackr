ba6f9186ebfc597af873e9cd95db948eadde2a2c
vickykumar1001 <vicky9060993097@gmail.com>
2024-11-03 11:37:56 +0530

    Updated auth routes 2

--- routes/auth.js
+++ routes/auth.js
@@ -5,14 +5,14 @@ const testUser = require('../middleware/testUser');
 const { register, verifyEmail, login, updateUser } = require('../controllers/auth')
 const rateLimiter = require('express-rate-limit');
 const apiLimiter = rateLimiter({
-  windowMs: 60 * 1000, // 15 minutes
-  max: 100000,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
   message: {
     msg: 'Too many requests from this IP, please try again after 15 minutes',
   },
 });
-router.post('/register', register)
-router.post('/verify-email', verifyEmail);
-router.post('/login', login)
+router.post('/register', apiLimiter, register)
+router.post('/verify-email', apiLimiter, verifyEmail);
+router.post('/login', apiLimiter, login)
 router.patch('/updateUser', authenticateUser, testUser, updateUser)
 module.exports = router
