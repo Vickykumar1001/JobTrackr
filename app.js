c0e42375c367a18f7575b31a5e2c7716910a6f17
vickykumar1001 <vicky9060993097@gmail.com>
2024-11-03 11:53:40 +0530

    added CORS and morgan

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const path = require('path');
 // extra security packages
 const helmet = require('helmet');
 const xss = require('xss-clean');
-
+const cors = require('cors');
 const express = require('express');
+const morgan = require("morgan");
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  credentials: true, // Allow credentials (e.g., cookies, authorization headers)
+}));
 
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
@@ -20,6 +26,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.set('trust proxy', 1);
 app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(morgan("tiny"));
 app.use(express.json());
 app.use(helmet());
 app.use(xss());
