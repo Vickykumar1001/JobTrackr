71f602d76771a34016019a0b5300e2dcb31e65d2
vickykumar1001 <vicky9060993097@gmail.com>
2024-11-03 11:52:07 +0530

    updated auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,37 +1,76 @@
+const crypto = require("crypto");
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
+const CustomError = require("../errors");
 const { BadRequestError, UnauthenticatedError } = require('../errors')
-
+const {
+  sendVerificationEmail,
+} = require("../utils");
+const { response } = require("express");
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body })
-  const token = user.createJWT()
+  const { email, name, password, location } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already registered");
+  }
+  const verificationToken = crypto.randomBytes(40).toString("hex");
+  const user = await User.create({ name, email, password, location, verificationToken });
+  const origin = "http://localhost:3000";
+  await sendVerificationEmail({
+    name: user.name,
+    email: user.email,
+    verificationToken: user.verificationToken,
+    origin,
+  });
   res.status(StatusCodes.CREATED).json({
-    user: {
-      email: user.email,
-      lastName: user.lastName,
-      location: user.location,
-      name: user.name,
-      token,
-    },
+    msg: "Success! Please check your email to verify account",
   });
 }
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+  console.log(verificationToken, email);
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError("Verification Failed");
+  }
+
+  (user.isVerified = true), (user.verified = Date.now());
+  user.verificationToken = "";
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Email Verified" });
+};
 
 const login = async (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
   if (!email || !password) {
-    throw new BadRequestError('Please provide email and password')
+    throw new BadRequestError('Please provide email and password');
   }
-  const user = await User.findOne({ email })
+
+  const user = await User.findOne({ email });
+  console.log('User found:', user); // Log user details to verify `isVerified` status
   if (!user) {
-    throw new UnauthenticatedError('Invalid Credentials')
+    throw new UnauthenticatedError('Invalid Credentials');
   }
-  const isPasswordCorrect = await user.comparePassword(password)
+
+  const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
-    throw new UnauthenticatedError('Invalid Credentials')
+    throw new UnauthenticatedError('Invalid Credentials');
   }
-  // compare password
-  const token = user.createJWT()
+
+  if (!user.isVerified) {
+    console.log('User is not verified'); // Additional log for verification status
+    throw new CustomError.UnauthenticatedError("Please verify your email");
+  }
+  const token = user.createJWT();
   res.status(StatusCodes.OK).json({
     user: {
       email: user.email,
@@ -41,9 +80,10 @@ const login = async (req, res) => {
       token,
     },
   });
-}
+};
+
+
 const updateUser = async (req, res) => {
-  console.log(req.user)
   const { email, name, lastName, location } = req.body;
   if (!email || !name || !lastName || !location) {
     throw new BadRequestError('Please provide all values');
@@ -70,6 +110,6 @@ const updateUser = async (req, res) => {
   });
 };
 module.exports = {
-  register,
+  register, verifyEmail,
   login, updateUser
 }
